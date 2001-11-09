e0c0e4c7152f15b8f9b3743623e1dad238cc9d3d
Yonatan Feldman <yon@milliped.com>
2001-11-09 02:42:26 +0000

    update

--- www/one-community-configure-2.tcl
+++ www/one-community-configure-2.tcl
@@ -11,28 +11,28 @@ ad_page_contract {
 
 
 set form [ns_getform]
-set portal_id [ns_set get $form portal_id]
+set page_id [ns_set get $form portal_id]
 
-# Check that this is a community type
-if {[ad_parameter community_level_p] != 1} {
-    ns_returnredirect "./"
+# Check if this is a community type level thing
+if {[ad_parameter community_type_level_p] == 1} {
+    ad_returnredirect "one-community-type"
     return
 }
 
-set user_id [ad_conn user_id]
-
-# What community type are we at?
-set community_id [dotlrn_community::get_community_id]
-
-# Check that this user is a member
-if {![dotlrn_community::member_p $community_id $user_id]} {
-    set context_bar [list "Not a member"]
-
-    ad_return_template one-community-not-member
+# Check if this is a community level thing
+if {[ad_parameter community_level_p] == 1} {
+    ad_returnredirect "one-community"
     return
-} else {
-
-    portal::configure_dispatch $portal_id $form
-
-    ns_returnredirect "one-community-configure"
 }
+
+# Make sure user is logged in
+set user_id [ad_maybe_redirect_for_registration]
+
+# If there is no page_id, this user is either a guest or something else
+if {![empty_string_p $page_id]} {
+    portal::configure_dispatch $page_id $form
+}
+
+ad_returnredirect "one-community-configure"
+
+
