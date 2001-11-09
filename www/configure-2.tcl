12f96edf636e3ee6a088802f4dc3d93bc7cf5abb
Yonatan Feldman <yon@milliped.com>
2001-11-09 03:57:18 +0000

    ci

---
+++ www/configure-2.tcl
@@ -0,0 +1,33 @@
+
+ad_page_contract {
+    A simple target for the portal configuration
+    
+    @author Arjun Sanyal (arjun@openforce.net)
+    @author Ben Adida (ben@openforce.net)
+    @creation-date 2001-10-24
+} {
+
+}
+
+
+set form [ns_getform]
+set page_id [ns_set get $form portal_id]
+
+# Check if this is a community type level thing
+if {[ad_parameter community_type_level_p] == 1} {
+    ad_returnredirect "one-community-type"
+    return
+}
+
+
+# Make sure user is logged in
+set user_id [ad_maybe_redirect_for_registration]
+
+# If there is no page_id, this user is either a guest or something else
+if {![empty_string_p $page_id]} {
+    portal::configure_dispatch $page_id $form
+}
+
+ad_returnredirect "configure"
+
+
