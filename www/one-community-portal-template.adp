bdd3ccf040ea95048315cd9d88abef574aa09931
Yonatan Feldman <yon@milliped.com>
2001-11-28 20:45:05 +0000

    new place with a new name

---
+++ www/one-community-portal-template.adp
@@ -0,0 +1,5 @@
+<master src="master">
+<property name="title">Configure this instance's template</property>
+<p>
+
+@rendered_page@
---
+++ www/one-community-portal-template.tcl
@@ -0,0 +1,14 @@
+# /dotlrn/www/admin/one-instance-portal-template.tcl
+ad_page_contract {
+    Configuration page for an instance's portal template
+
+    @author Arjun Sanyal (arjun@openforce.net)
+    @cvs-id $Id$
+} {
+    portal_id:naturalnum,notnull
+    return_url:notnull
+}
+
+set rendered_page [portal::template_configure $portal_id $return_url]
+
+ad_return_template
