5f0049e5972ddec249ab95146601b5545d9d0bbe
Yonatan Feldman <yon@milliped.com>
2001-12-04 20:53:53 +0000

    truly checking for an instance of dotlrn and not an installed dotlrn package!

--- tcl/dotlrn-init.tcl
+++ tcl/dotlrn-init.tcl
@@ -17,31 +17,33 @@ ad_library {
     
 }
 
-# We go through all Applets and make sure they are added.
-
-# The applet_add proc in the dotlrn_applet contract is for one-time
-# init of each applet NOTE: this applet_add proc _must_ be able to be
-# called repeatedly since this script is eval'd at every server startup
-# - aks
-
-foreach applet [dotlrn_community::list_applets] {
-    # Callback on all applets
-    dotlrn_community::applet_call $applet "AppletAdd" [list]
-}
 
 # We check to see if dotLRN has been installed, and if so, if permissions
 # have been granted
 
-set main_package_id [dotlrn::get_package_id]
 
 # if installed
-if {![empty_string_p $main_package_id]} {
+if {[dotlrn::is_instantiated]} {
     # Grantee
     set grantee_id [dotlrn::get_full_users_rel_segment_id]
 
+    set package_id [dotlrn::get_package_id]
+
     # Grant the permission
-    if {![ad_permission_p -user_id $grantee_id $main_package_id dotlrn_browse]} {
-	ad_permission_grant $grantee_id $main_package_id dotlrn_browse
+    if {![ad_permission_p -user_id $grantee_id $package_id dotlrn_browse]} {
+	ad_permission_grant $grantee_id $package_id dotlrn_browse
+    }
+
+    # We go through all Applets and make sure they are added.
+
+    # The applet_add proc in the dotlrn_applet contract is for one-time
+    # init of each applet NOTE: this applet_add proc _must_ be able to be
+    # called repeatedly since this script is eval'd at every server startup
+    # - aks
+    
+    foreach applet [dotlrn_community::list_applets] {
+	# Callback on all applets
+	dotlrn_community::applet_call $applet "AddApplet" [list]
     }
 }
 
