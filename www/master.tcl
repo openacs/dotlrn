975d45acb20d55cb00da5de143afce823b68dfe5
Yonatan Feldman <yon@milliped.com>
2002-01-21 09:35:39 +0000

    update

--- www/master.tcl
+++ www/master.tcl
@@ -7,11 +7,12 @@ if {![empty_string_p $portal_id]} {
     if {[portal::page_count -portal_id $portal_id] > 1} {
         set navbar "<table border=0 cellpadding=5><tr>"
     
-        append navbar [portal::list_pages -portal_id $portal_id -link "foobarlink" -pre_html "<td><font face=arial,helvetica size=-1 color=black><b> <center>" -separator " </center></td> <td><font face=arial,helvetica size=-1 color=black><b> <center>" -post_html "</a></center></td>"]
+        append navbar [portal::list_pages -portal_id $portal_id -link "switch-page" -pre_html "<td><font face=arial,helvetica size=-1 color=black><b> <center>" -separator " </center></td> <td><font face=arial,helvetica size=-1 color=black><b> <center>" -post_html "</a></center></td>"]
         
         append navbar "</tr></table>"
-    } 
-    set navbar ""
+    } else {
+        set navbar ""
+    }
 } else {
     set navbar ""
 }
