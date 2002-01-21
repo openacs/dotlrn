0c8bb45abbbabaf3e908d585859f265a3ec5fa00
Yonatan Feldman <yon@milliped.com>
2002-01-21 07:42:50 +0000

    ci

---
+++ www/master.tcl
@@ -0,0 +1,40 @@
+#
+# sloan specifc master
+#
+
+# if page count > 1, show navbar
+if {![empty_string_p $portal_id]} {
+    if {[portal::page_count -portal_id $portal_id] > 1} {
+        set navbar "<table border=0 cellpadding=5><tr>"
+    
+        append navbar [portal::list_pages -portal_id $portal_id -link "foobarlink" -pre_html "<td><font face=arial,helvetica size=-1 color=black><b> <center>" -separator " </center></td> <td><font face=arial,helvetica size=-1 color=black><b> <center>" -post_html "</a></center></td>"]
+        
+        append navbar "</tr></table>"
+    } 
+    set navbar ""
+} else {
+    set navbar ""
+}
+
+set user_id [ad_verify_and_get_user_id]
+
+db_0or1row pvt_home_user_info {
+    select first_names, last_name
+    from cc_users 
+    where user_id=:user_id
+}
+
+set full_name "$first_names $last_name"
+set title "$full_name : MySloanSpace"
+
+if {[ad_parameter community_level_p] == 1} {
+    # in a community
+    set text [dotlrn_community::get_community_name [dotlrn_community::get_community_id]]
+
+} elseif {[ad_parameter community_type_level_p] == 1} {
+    # in a community type
+    set text [dotlrn_community::get_community_type_name [dotlrn_community::get_community_type]]
+} else {
+    # under /dotlrn
+    set text $full_name
+}
