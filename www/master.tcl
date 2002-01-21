7171dea0c0ab6ffa00f463debf7c66b26156e74e
Yonatan Feldman <yon@milliped.com>
2002-01-21 22:08:46 +0000

    more page updates

--- www/master.tcl
+++ www/master.tcl
@@ -2,17 +2,17 @@
 # sloan specifc master
 #
 
-# if page count > 1, show navbar
+# if we have a portal_id show navbar
 if {![empty_string_p $portal_id]} {
-    if {[portal::page_count -portal_id $portal_id] > 1} {
+#    if {[portal::page_count -portal_id $portal_id] > 1} {
         set navbar "<table border=0 cellpadding=5><tr>"
     
         append navbar [portal::list_pages -portal_id $portal_id -link "switch-page" -pre_html "<td><font face=arial,helvetica size=-1 color=black><b> <center>" -separator " </center></td> <td><font face=arial,helvetica size=-1 color=black><b> <center>" -post_html "</a></center></td>"]
         
         append navbar "</tr></table>"
-    } else {
-        set navbar ""
-    }
+#    } else {
+#        set navbar ""
+#    }
 } else {
     set navbar ""
 }
