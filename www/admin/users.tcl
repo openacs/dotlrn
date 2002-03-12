a9846cdfa43bb5aae510b340026562313f47226a
Yonatan Feldman <yon@milliped.com>
2002-03-12 16:42:00 +0000

    added counts to user type control bar

--- www/admin/users.tcl
+++ www/admin/users.tcl
@@ -15,22 +15,28 @@ ad_page_contract {
 set context_bar {Users}
 
 set dotlrn_roles [db_list_of_lists select_dotlrn_roles {
-    select type,
-           pretty_name,
+    select dotlrn_user_types.type,
+           dotlrn_user_types.pretty_name || ' (' || (select count(*)
+                                                     from party_member_map
+                                                     where party_member_map.party_id = dotlrn_user_types.group_id
+                                                     and party_member_map.member_id <> dotlrn_user_types.group_id) || ')',
            ''
     from dotlrn_user_types
-    order by pretty_name
+    order by dotlrn_user_types.pretty_name
 }]
 
-lappend dotlrn_roles {pending Pending {}}
-lappend dotlrn_roles {deactivated Deactivated {}}
+set n_pending_users [db_string select_non_dotlrn_users_count {}]
+lappend dotlrn_roles [list pending "Pending ($n_pending_users)" {}]
+
+set n_deactivated_users [db_string select_deactivated_users_count {}]
+lappend dotlrn_roles [list deactivated "Deactivated ($n_deactivated_users)" {}]
 
 set control_bar [ad_dimensional [list [list type {User Type:} admin $dotlrn_roles]]]
 
 if {[string equal $type "deactivated"] == 1} {
-    set n_users [db_string select_deactivated_users_count {}]
+    set n_users $n_deactivated_users
 } elseif {[string equal $type "pending"] == 1} {
-    set n_users [db_string select_non_dotlrn_users_count {}]
+    set n_users $n_pending_users
 } else {
     set n_users [db_string select_dotlrn_users_count {}]
 }
