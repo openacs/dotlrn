2d4d60f8b0ea2b2a4f528d0e8f3e2dbe9b07affe
Arjun Sanyal <arjun@openforce.net>
2002-03-14 23:46:35 +0000

    fixed it so that users that can admin this comm but aren't members get a nice navbar, so they don't get lost

--- www/members.tcl
+++ www/members.tcl
@@ -11,5 +11,13 @@ set community_id [dotlrn_community::get_community_id]
 set user_id [ad_get_user_id]
 set context_bar {{"one-community-admin" Admin} {Manage Members}}
 set portal_id [dotlrn_community::get_portal_id $community_id $user_id]
+set admin_p [dotlrn::user_can_admin_community_p -user_id $user_id $community_id]
+
+if {[empty_string_p $portal_id] && $admin_p} {
+    # the admin using this page is not a member of this comm, but they
+    # need he contol panel link (they will probably be confused as to 
+    # where they are, so...
+    set portal_id [dotlrn_community::get_community_admin_portal_id $community_id]
+}
 
 ad_return_template
