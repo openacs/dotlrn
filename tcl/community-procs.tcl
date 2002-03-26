ce959b1d0ba19d9c256ee71f464de88aedc08f09
Arjun Sanyal <arjun@openforce.net>
2002-03-26 20:21:29 +0000

    improved get_subcomm_chunk to handle pending memberships

--- tcl/community-procs.tcl
+++ tcl/community-procs.tcl
@@ -930,25 +930,31 @@ namespace eval dotlrn_community {
 
                 set url [get_community_url $sc_id]
 
-                append chunk "$pretext <a href=$url>[get_community_name $sc_id]</a>"
+                append chunk "$pretext <a href=$url>[get_community_name $sc_id]</a>\n"
 
-                if {![member_p $sc_id $user_id] && [not_closed_p -community_id $sc_id]} {
+                if {![member_p $sc_id $user_id] 
+                  && [not_closed_p -community_id $sc_id] 
+                  || [member_pending_p -community_id $sc_id -user_id $user_id]} {
                     # don't show the join link if the comm is closed, 
                     # and the first space is good below
                     append chunk \
-                            " <small>\[<a href=${url}${join_target}>"
+                            "<small>\[<a href=${url}${join_target}>"
                     if {[needs_approval_p -community_id $sc_id]} {
-                        append chunk "request&nbsp;membership" 
+                        if {[member_pending_p -community_id $sc_id -user_id $user_id]} {
+                            append chunk "waiting&nbsp;for&nbsp;approval" 
+                        } else {
+                            append chunk "request&nbsp;membership" 
+                        }
                     } else {
                         append chunk "join"
                     } 
 
-                    append chunk "</a>\]</small>"
+                    append chunk "</a>\]</small>\n"
                 }
 
                 if {[dotlrn::user_can_admin_community_p $sc_id]} {
                     append chunk \
-                            " <small>\[<a href=${url}one-community-admin>admin</a>\]</small>"
+                            " <small>\[<a href=${url}one-community-admin>admin</a>\]</small>\n"
                 }
             }
         }
