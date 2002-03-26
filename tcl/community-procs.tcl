40beaeb4516c837797cc6d292dda1d79a6929b1b
Arjun Sanyal <arjun@openforce.net>
2002-03-26 20:54:41 +0000

    Changed the referer for the get_subcomm_chunk "register" link to the current location. So when you hit the link, go don't go anywhere.

--- tcl/community-procs.tcl
+++ tcl/community-procs.tcl
@@ -938,7 +938,7 @@ namespace eval dotlrn_community {
                     # don't show the join link if the comm is closed, 
                     # and the first space is good below
                     append chunk \
-                            "<small>\[<a href=${url}${join_target}>"
+                            "<small>\[<a href=${url}${join_target}?referer=[ad_conn url]>"
                     if {[needs_approval_p -community_id $sc_id]} {
                         if {[member_pending_p -community_id $sc_id -user_id $user_id]} {
                             append chunk "waiting&nbsp;for&nbsp;approval" 
