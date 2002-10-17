88a7577792bbe968ad89a64c767d11f92ed19a22
Jeff Davis <davis@xarg.net>
2002-10-17 12:27:19 +0000

    sloan - paramater for admin_page_name, markup cleanup, allow users to modify own portal

--- www/configure.adp
+++ www/configure.adp
@@ -18,10 +18,21 @@
 
 %>
 
-<master src="dotlrn-master">
-<property name="title">Customize Layout</property>
-<property name="link_control_panel">0</property>
+<master>
+<property name="title">Configure @name@</property>
+<property name="portal_id">@portal_id@</property>
+<property name="link_all">1</property>
+<property name="show_control_panel">1</property>
+<property name="link_control_panel">1</property>
+<property name="return_url">./</property>
 
 <p></p>
 
 @rendered_page@
+
+
+
+
+
+
+
--- www/configure.tcl
+++ www/configure.tcl
@@ -44,25 +44,14 @@ if {[parameter::get -parameter community_type_level_p] == 1} {
 } else {
     set user_id [ad_conn user_id]
 
-    # am i not a limited access user with 0 or 1 communities?
-    # if so, i have a user portal that i can configure
-    if {![dotlrn::user_can_browse_p -user_id $user_id]} {
-    
-        set communities [dotlrn_community::get_all_communities_by_user $user_id]
-    
-        if {[llength $communities] == 0} {
-            ad_returnredirect "index-not-a-user"
-            ad_script_abort
-        } elseif {[llength $communities] == 1} {
-            ad_returnredirect [ns_set get [lindex $communities 0] url]
-            ad_script_abort
-        }
-    
-    }
+    # I don't see under what circumstance we wouldn't want users to
+    # customize thier own portal. -Caroline.
+
+    #    dotlrn::require_user_browse -user_id $user_id
 
     set portal_id [dotlrn::get_portal_id -user_id $user_id]
     set name [portal::get_name $portal_id]
-    set rendered_page [portal::configure $portal_id "index"]
+    set rendered_page [portal::configure -allow_theme_change_p 0 $portal_id "index"]
 }
 
 ad_return_template
