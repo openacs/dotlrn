f04ecd4003001781bed94a53f1e23bb9bc791bc9
Janine Sisk <janine@furfly.net>
2003-02-03 03:54:33 +0000

    moved unported Oracle query out to xql files

--- www/spam.tcl
+++ www/spam.tcl
@@ -79,7 +79,7 @@ element create spam_message send_date \
     -label {Send Date} \
     -datatype date \
     -widget date \
-    -format {MONTH DD YYYY HH12:MI AM} \
+    -format {MONTH DD YYYY HH12 MI AM} \
     -value [template::util::date::now]
 
 element create spam_message referer \
@@ -98,49 +98,7 @@ if {[ns_queryexists "form:confirm"]} {
 
     set safe_community_name [db_quote $community_name]
 
-    set query "
-        select '$from' as from_addr,
-               '$sender_first_names' as sender_first_names,
-               '$sender_last_name' as sender_last_name,
-               parties.email,
-               decode(acs_objects.object_type,
-                      'user',
-                      (select first_names
-                       from persons
-                       where person_id = parties.party_id),
-                      'group',
-                      (select group_name
-                       from groups
-                       where group_id = parties.party_id),
-                      'rel_segment',
-                      (select segment_name
-                       from rel_segments
-                       where segment_id = parties.party_id),
-                      '') as first_names,
-               decode(acs_objects.object_type,
-                      'user',
-                      (select last_name
-                       from persons
-                       where person_id = parties.party_id),
-                      '') as last_name,
-               '$safe_community_name' as community_name,
-               '$community_url' as community_url
-            from party_approved_member_map,
-                 parties,
-                 acs_objects
-            where party_approved_member_map.party_id = $segment_id
-            and party_approved_member_map.member_id <> $segment_id
-            and party_approved_member_map.member_id = parties.party_id
-            and parties.party_id = acs_objects.object_id
-            and parties.party_id in (select acs_rels.object_id_two  
-                                     from acs_rels, membership_rels
-                                     where acs_rels.object_id_one = 
-                                        acs.magic_object_id('registered_users')
-                                     and acs_rels.rel_id = 
-                                        membership_rels.rel_id
-                                     and membership_rels.member_state 
-                                        = 'approved')
-    "
+    set query [db_map sender_info]
 
 ns_log notice "query: $query"
 
