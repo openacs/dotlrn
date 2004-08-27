73e6b52e6009f54e333de91271de5060534f41ff
Tracy Adams <teadams@alum.mit.edu>
2004-08-27 16:12:10 +0000

    context bar only for ShowContextBarP

--- www/member-add.tcl
+++ www/member-add.tcl
@@ -32,7 +32,6 @@ set community_id [dotlrn_community::get_community_id]
 # Just search
 db_multirow users select_users {}
 
-
 set context_bar [list [list "one-community-admin" [_ dotlrn.Admin]] [_ dotlrn.New_User]]
 
 
