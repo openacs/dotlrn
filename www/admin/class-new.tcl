8608779081eb130c3f96647f600407d1a6b96fc8
Malte Sussdorff <openacs@sussdorff.de>
2005-05-11 12:12:25 +0000

    Still problems with the community_key

--- www/admin/class-new.tcl
+++ www/admin/class-new.tcl
@@ -78,7 +78,7 @@ if {[form is_valid add_class]} {
 
     # If we do not provide a special subject_key make use of the pretty_name instead
     if {[empty_string_p $subject_key]} {
-	set subject_key $pretty_name
+	set subject_key $department_key.[dotlrn::generate_key -name $pretty_name]
     }
 
     set class_key [dotlrn_class::new \
