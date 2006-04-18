4c169157b4628c8ca1cf5394049ad6cd2f9040d6
Don Baccus <dhogaza@pacifier.com>
2006-04-18 23:48:39 +0000

    1. Modify install.xml to install the Selva theme. Sloan theme is still the    default theme when .LRN is installed, but you can now use the site template    admin pages to make selva your theme.
    2. Hid Kelp theme from view.  It's still installed as a portal theme, but if
       you want to make it visible to the site template stuff you need to add
       it via the "add site template" web UI page.

--- sql/oracle/site-templates-create.sql
+++ sql/oracle/site-templates-create.sql
@@ -24,19 +24,6 @@ declare
 	v_site_template_id	dotlrn_site_templates.site_template_id%TYPE;
 	v_theme_id		portal_element_themes.theme_id%TYPE;
 begin
-	select theme_id into v_theme_id 
-        from portal_element_themes 
-	where name = 'KELP'; 
-	
-	select acs_object_id_seq.nextval 
-        into v_site_template_id 
-        from dual;
-
-	insert into dotlrn_site_templates
-	(site_template_id, pretty_name, site_master, portal_theme_id ) 
-	values 
-	(v_site_template_id, 'KELP','/packages/dotlrn/www/dotlrn-master-kelp', v_theme_id);
-
 
         select theme_id into v_theme_id 
         from portal_element_themes 
--- sql/postgresql/site-templates-create.sql
+++ sql/postgresql/site-templates-create.sql
@@ -71,20 +71,6 @@ declare
 	v_site_template_id	dotlrn_site_templates.site_template_id%TYPE;
 	v_theme_id		portal_element_themes.theme_id%TYPE;
 begin
-	select theme_id into v_theme_id 
-        from portal_element_themes 
-	where name = ''#new-portal.kelp_theme_name#''; 
-	
-	select acs_object_id_seq.nextval 
-        into v_site_template_id 
-        from dual;
-
-	insert into dotlrn_site_templates
-	(site_template_id, pretty_name, site_master, portal_theme_id ) 
-	values 
-	(v_site_template_id, ''#new-portal.kelp_theme_name#'',''/packages/dotlrn/www/dotlrn-master-kelp'', v_theme_id);
-
-
         select theme_id into v_theme_id 
         from portal_element_themes 
 	where name = ''#new-portal.sloan_theme_name#''; 
