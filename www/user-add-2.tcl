f2b3ad647e0db60bde91c5beadb3c1b2612a2ae3
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2019-11-28 11:06:32 +0000

    don't pass sensitive information (e.g. password) as query variable, but use client properties instead.
    see also issue #3344

--- www/user-add-2.tcl
+++ www/user-add-2.tcl
@@ -68,7 +68,12 @@ if {!$dotlrn_user_p} {
     }
 }
 
-set redirect [export_vars -base user-add-2 {user_id password referer type can_browse_p read_private_data_p dotlrn_interactive_p add_membership_p}]
+if {[info exists password]} {
+    security::set_client_property_password $password
+}
+set redirect [export_vars -base user-add-2 {
+    user_id referer type can_browse_p read_private_data_p dotlrn_interactive_p add_membership_p
+}]
 if { $add_membership_p == "t" && $referer eq "/acs-admin/users" } {
     set redirect "one-community-admin"
 } else {
