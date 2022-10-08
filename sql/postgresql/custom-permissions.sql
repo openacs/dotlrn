1b845ba0a1334472978adc8c74137e571b871c28
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-10-08 20:07:17 +0000

    Cleanup of acs_object_context_index' and friends in creation scripts
    This change completes the commit [1] from 2019, where the cleanup was performed
    acs_object_context_index and its maintenance functions just as an update script.
    This change addresses this also in the creation scripts and in packages, still
    depending on the old functions.
    
    [1] https://fisheye.openacs.org/changelog/OpenACS?cs=oacs-5-10%3Ahectorr%3A20190708160122

--- sql/postgresql/custom-permissions.sql
+++ sql/postgresql/custom-permissions.sql
@@ -22,50 +22,51 @@
 -- If you customize the dotLRN community datamodel and violate any of the
 -- above assumptions, rewrite this function.
 
-
-
-
--- added
-select define_function_args('dotlrn_community_admin_p','group_id,party_id');
+-- This function is nowhere called, so deactivate this for now, but
+-- keep it for documentation purposes. The handling of direct
+-- permissions should be done via permission::permission_p, the
+-- handling of the relsegments should be doable over this as well.
+--
+-- select define_function_args('dotlrn_community_admin_p','group_id,party_id');
 
 --
 -- procedure dotlrn_community_admin_p/2
 --
-CREATE OR REPLACE FUNCTION dotlrn_community_admin_p(
-   p_group_id integer,
-   p_party_id integer
-) RETURNS char AS $$
-DECLARE 
-  BEGIN
-    --
-    -- direct permissions
-    if exists (
-        select 1
-          from acs_object_grantee_priv_map
-         where object_id = p_group_id
-           and grantee_id =  p_party_id
-           and privilege = 'admin')
-    then 
-        return 't';
-    end if;       
-
-    -- check to see if the user belongs to a rel seg that has
-    -- the admin priv on the object (in this case a group)
-
-
-    if exists (
-        select 1
-        from acs_object_grantee_priv_map ogpm,
-          rel_seg_approved_member_map rs
-        where rs.group_id = p_group_id
-          and ogpm.object_id = rs.group_id
-          and ogpm.privilege = 'admin'
-          and ogpm.grantee_id = rs.segment_id
-          and rs.member_id = p_party_id)
-    then
-        return 't';
-    end if;
-
-    return 'f';
-END;
-$$ LANGUAGE plpgsql;
+-- CREATE OR REPLACE FUNCTION dotlrn_community_admin_p(
+--    p_group_id integer,
+--    p_party_id integer
+-- ) RETURNS char AS $$
+-- DECLARE 
+--   BEGIN
+--     --
+--     -- direct permissions
+--     if exists (
+--         select 1
+--           from acs_object_grantee_priv_map
+--          where object_id = p_group_id
+--            and grantee_id =  p_party_id
+--            and privilege = 'admin')
+--     then 
+--         return 't';
+--     end if;       
+-- 
+--     -- check to see if the user belongs to a rel seg that has
+--     -- the admin priv on the object (in this case a group)
+-- 
+-- 
+--     if exists (
+--         select 1
+--         from acs_object_grantee_priv_map ogpm,
+--           rel_seg_approved_member_map rs
+--         where rs.group_id = p_group_id
+--           and ogpm.object_id = rs.group_id
+--           and ogpm.privilege = 'admin'
+--           and ogpm.grantee_id = rs.segment_id
+--           and rs.member_id = p_party_id)
+--     then
+--         return 't';
+--     end if;
+-- 
+--     return 'f';
+-- END;
+-- $$ LANGUAGE plpgsql;
